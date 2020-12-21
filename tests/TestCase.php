b12445a2ddf53daa539d3e73d48f7905e4086217
Simon Frings <simon@clue.engineering>
2020-12-21 14:28:00 +0100

    Run tests on PHPUnit 9

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -39,6 +39,12 @@ class TestCase extends \PHPUnit\Framework\TestCase
 
     protected function createCallableMock()
     {
-        return $this->getMockBuilder('stdClass')->setMethods(array('__invoke'))->getMock();
+        if (method_exists('PHPUnit\Framework\MockObject\MockBuilder', 'addMethods')) {
+            // PHPUnit 8.5+
+            return $this->getMockBuilder('stdClass')->addMethods(array('__invoke'))->getMock();
+        } else {
+            // legacy PHPUnit 4 - PHPUnit 8.4
+            return $this->getMockBuilder('stdClass')->setMethods(array('__invoke'))->getMock();
+        }
     }
 }
